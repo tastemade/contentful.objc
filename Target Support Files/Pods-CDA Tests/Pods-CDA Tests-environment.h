bb8c781339c51fc51f3a693202b3083df0b02df2
Boris Bügling <boris@icculus.org>
2015-03-12 10:30:05 +0100

    Initially commit Pods/ from master

---
+++ Target Support Files/Pods-CDA Tests/Pods-CDA Tests-environment.h
@@ -0,0 +1,74 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// CCLRequestReplay
+#define COCOAPODS_POD_AVAILABLE_CCLRequestReplay
+#define COCOAPODS_VERSION_MAJOR_CCLRequestReplay 0
+#define COCOAPODS_VERSION_MINOR_CCLRequestReplay 9
+#define COCOAPODS_VERSION_PATCH_CCLRequestReplay 0
+
+// CCLRequestReplay/Blueprint
+#define COCOAPODS_POD_AVAILABLE_CCLRequestReplay_Blueprint
+#define COCOAPODS_VERSION_MAJOR_CCLRequestReplay_Blueprint 0
+#define COCOAPODS_VERSION_MINOR_CCLRequestReplay_Blueprint 9
+#define COCOAPODS_VERSION_PATCH_CCLRequestReplay_Blueprint 0
+
+// CCLRequestReplay/JSON
+#define COCOAPODS_POD_AVAILABLE_CCLRequestReplay_JSON
+#define COCOAPODS_VERSION_MAJOR_CCLRequestReplay_JSON 0
+#define COCOAPODS_VERSION_MINOR_CCLRequestReplay_JSON 9
+#define COCOAPODS_VERSION_PATCH_CCLRequestReplay_JSON 0
+
+// CCLRequestReplay/Manager
+#define COCOAPODS_POD_AVAILABLE_CCLRequestReplay_Manager
+#define COCOAPODS_VERSION_MAJOR_CCLRequestReplay_Manager 0
+#define COCOAPODS_VERSION_MINOR_CCLRequestReplay_Manager 9
+#define COCOAPODS_VERSION_PATCH_CCLRequestReplay_Manager 0
+
+// CCLRequestReplay/Record
+#define COCOAPODS_POD_AVAILABLE_CCLRequestReplay_Record
+#define COCOAPODS_VERSION_MAJOR_CCLRequestReplay_Record 0
+#define COCOAPODS_VERSION_MINOR_CCLRequestReplay_Record 9
+#define COCOAPODS_VERSION_PATCH_CCLRequestReplay_Record 0
+
+// CCLRequestReplay/Replay
+#define COCOAPODS_POD_AVAILABLE_CCLRequestReplay_Replay
+#define COCOAPODS_VERSION_MAJOR_CCLRequestReplay_Replay 0
+#define COCOAPODS_VERSION_MINOR_CCLRequestReplay_Replay 9
+#define COCOAPODS_VERSION_PATCH_CCLRequestReplay_Replay 0
+
+// FBSnapshotTestCase
+#define COCOAPODS_POD_AVAILABLE_FBSnapshotTestCase
+#define COCOAPODS_VERSION_MAJOR_FBSnapshotTestCase 1
+#define COCOAPODS_VERSION_MINOR_FBSnapshotTestCase 5
+#define COCOAPODS_VERSION_PATCH_FBSnapshotTestCase 0
+
+// OCMock
+#define COCOAPODS_POD_AVAILABLE_OCMock
+#define COCOAPODS_VERSION_MAJOR_OCMock 2
+#define COCOAPODS_VERSION_MINOR_OCMock 2
+#define COCOAPODS_VERSION_PATCH_OCMock 4
+
+// Realm
+#define COCOAPODS_POD_AVAILABLE_Realm
+#define COCOAPODS_VERSION_MAJOR_Realm 0
+#define COCOAPODS_VERSION_MINOR_Realm 88
+#define COCOAPODS_VERSION_PATCH_Realm 0
+
+// Realm/Headers
+#define COCOAPODS_POD_AVAILABLE_Realm_Headers
+#define COCOAPODS_VERSION_MAJOR_Realm_Headers 0
+#define COCOAPODS_VERSION_MINOR_Realm_Headers 88
+#define COCOAPODS_VERSION_PATCH_Realm_Headers 0
+
+// VCRURLConnection
+#define COCOAPODS_POD_AVAILABLE_VCRURLConnection
+#define COCOAPODS_VERSION_MAJOR_VCRURLConnection 0
+#define COCOAPODS_VERSION_MINOR_VCRURLConnection 2
+#define COCOAPODS_VERSION_PATCH_VCRURLConnection 0
+
